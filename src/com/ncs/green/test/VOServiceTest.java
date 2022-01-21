67a629d2cebd1c50df490a50bb0215b5b82021d1
hj9 <gyqkf96@gmail.com>
2022-01-21 15:36:51 +0900

    first commit test

---
+++ src/com/ncs/green/test/VOServiceTest.java
@@ -0,0 +1,26 @@
+package com.ncs.green.test;
+
+import com.ncs.green.service.VOService;
+import com.ncs.green.vo.MemberVO;
+
+public class VOServiceTest {
+	
+	public static void main(String[] args) {
+		// 1) MemberVO 생성
+		MemberVO vo = new MemberVO();
+		vo.setId("banana");
+		vo.setPassword("12345!");
+		vo.setName("홍길동");
+		
+		// 2) 직접 출력
+		System.out.println("** 직접 출력 => "+vo);
+		
+		// 3) VOService로 출력하기
+		VOService service = new VOService();
+		service.setMemberVO(vo);
+		System.out.println("** AntBuild Test **");
+		System.out.println("** VOService 출력 => "+service.getMemberVO());
+		
+	} //main
+
+} //class
